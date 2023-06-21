630b9cf9e7c87438083a35a7e890461bdd48bdeb
prd-hieu-nguyen-minh <hieu.nguyen.minh@paradox.ai>
2023-06-21 17:57:04 +0700

    Update code

--- app/src/main/java/com/hieuminh/toasterlibrary/MainActivity.kt
+++ app/src/main/java/com/hieuminh/toasterlibrary/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.hieuminh.toasterlibrary
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hieuminh.toasterlib.Toaster
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toaster.showToast(this, "ABCzdfsdf sdf sdff sdf sdf sdf")
     }
 }
